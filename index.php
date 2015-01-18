1ab83cc72e5e8d89c3b739e51d827a3dc649616f
Andre Rainaud <ARD@users.noreply.github.com>
2015-01-17 23:36:38 -0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ require 'SimpleHTTPAuth.class.php';
 
 $auth = new SimpleHTTPAuth();
 $auth->addUser('teste', '123456');
+
+/*
+$auth->addUser('usuario2', '123456')
+    ->addUser('usuario3','123456');
+*/
+
 $auth->run();
 
 echo 'Autenticado!';
